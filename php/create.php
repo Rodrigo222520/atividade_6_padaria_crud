f340350bc890afc8e2aaf0dda1da8075a9866227
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-08-12 12:52:34 -0300

    add

--- php/create.php
+++ php/create.php
@@ -7,11 +7,8 @@
 </head>
 <body>
     <h1>Bem vindo!</h1>
-    <a href="">
-<<<<<<< HEAD
-        <img class="diminuirImagem" src="../assets/icons/pao-frances-12h-gg.png.webp">
-=======
-        <img src="">
->>>>>>> ceac5c53bc09e81f7c1591c2e7540f4c0545e50d
+    
+        <img class="diminuirImagem" src="../assets/icons/Pao.webp">
+        <img class="diminuirImagem" src="../assets/icons/Bolo-de-Chocolate.webp">
 </body>
 </html>
