9b32a6bbace51448c49657eaf13b358c5c5210f4
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-08-12 12:26:55 -0300

    Adicionado uma imagem ao css.

--- php/create.php
+++ php/create.php
@@ -2,11 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Create</title>
 </head>
 <body>
+    <h1>Bem vindo!</h1>
     <a href="">
-        <img src="
+        <img class="diminuirImagem" src="../assets/icons/pao-frances-12h-gg.png.webp">
 </body>
 </html>
