afde3468a6006d9cfe3781d959bfe5a02dd79f8e
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-08-14 10:06:46 -0300

    Feito o inicio do background padrão do header.

--- public/produtos.php
+++ public/produtos.php
@@ -13,8 +13,17 @@
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
+<header class="header_background">
 
-    
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
 </body>
 </html>
 
