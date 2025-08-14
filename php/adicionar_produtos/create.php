a9f23b50f1fb134598096e01540dfb0c387c1f2a
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-08-14 19:56:50 -0300

    Add formulário para o preço em adiconar produtos e imagens.

--- php/adicionar_produtos/create.php
+++ php/adicionar_produtos/create.php
@@ -2,14 +2,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/style.css">
     <title>Produtos</title>
 </head>
 <body>
-    <header class="header_background">
+<footer>
+<div class="header_background">
         <a href="">
             <img class="header_esquerda" src="../assets/icons/setaMarrom.png" alt="imagem seta marrom">
             <img src="../assets/logos/Logos.png">
         </a>
-    </header>
+</div>
+</footer>
+<br>
+<main>
+    <div class="quadradoMarrom">
+        <input type="text" name="R$6,50">
+        <h2>Pastel<h2>
+        <h4>Feito na hora com uma receita de família<h4>
+        <img src="mais.png">
+    <div>
+    
+
 </body>
 </html>
