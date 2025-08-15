725edda2d4d31868f25d769d33d981359685f53c
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-08-15 09:38:07 -0300

    Mudado o nome dos aruivos novamente para snake case.

--- php/adicionar_pedidos/create_pedidos.php
+++ php/adicionar_pedidos/create_pedidos.php
@@ -6,14 +6,14 @@
     <title>Create</title>
 </head>
 <body>
-    <img class="diminuirImagem" src="../../assets/logos/Logos.png">
+    <img class="diminuir_imagem" src="../../assets/logos/Logos.png">
 </body>
 
 <footer>
      <div class="menu">
         <a href="">
-            <button class="botaoMenu">
-            <img class="imagemProdutos" src="../../assets/icons/imagemCarrinho.webp" alt="">
+            <button class="botao_menu">
+            <img class="imagem_produtos" src="../../assets/icons/imagemCarrinho.webp" alt="">
             <div>
                 <h4>Produtos</h4>
             </div>
@@ -21,8 +21,8 @@
         </a>
         
         <a href="">
-             <button class="botaoMenu">
-            <img class="imagemProdutos" src="../../assets/icons/imagemCliente.webp" alt="">
+             <button class="botao_menu">
+            <img class="imagem_produtos" src="../../assets/icons/imagemCliente.webp" alt="">
             <div>
                 <h4>Clientes</h4>
             </div>
@@ -30,8 +30,8 @@
         </a>
        
         <a href="">
-            <button class="botaoMenu" style= "padding-top: 4px" >
-            <img class="imagemProdutos" src="../../assets/icons/imagemPedidos.webp" alt="">
+            <button class="botao_menu" style= "padding-top: 4px" >
+            <img class="imagem_produtos" src="../../assets/icons/imagemPedidos.webp" alt="">
             <div>
                 <h4>Pedidos</h4>
             </div>
---
+++ php/adicionar_produtos/create_produtos.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/style.css">
+    <title>Produtos</title>
+</head>
+<body>
+<footer>
+<div class="header_background">
+        <img class="header_esquerda" src="../../assets/logos/logo.webp">
+</div>
+</footer>
+<br>
+<main>
+    <div class="quadrado_marrom">
+        <label for="Preco">Preço:</label>
+        <input type="text" class="form_produto"><br><br>
+        <label for="Produto">Produto:</label>
+        <input type="text" class="form_produto">
+        <img class="diminuir_imagem2" src="../../assets/icons/mais.webp">
+    <div>
+    
+
+</body>
+</html>
