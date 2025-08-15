24a6caa6d737ea0c055efb052f6269cf84aabe58
NarutoKyochi <gabrielkyochi@gmail.com>
2025-08-15 19:57:07 -0300

    Arrumado os botões de ver carrinho e adicionar produtos.

--- public/produtos.php
+++ public/produtos.php
@@ -14,18 +14,23 @@
 </head>
 <body>
 <header>
-<div  class="header_background">
+<div class="header_background">
     <div class="posicao_seta">
         <img src="../assets/icons/seta_voltar.png" alt="Seta de voltar">
     </div>
 <img class="tamanho_logo" src="../assets/logos/logo_padaria.png" alt="Logo da padaria">
-<div class="botao_ver_carrinho">
+<a href="">
+<button id="botao_ver_carrinho">
 <img id="tamanho_icone_carrinho" src="../assets/icons/carrinho.png" alt="Carrinho">
 <p>Ver carrinho</p>
+</button>
+</a>
 </div>
-<div class="botao_adicionar_produtos">
+<a href="">
+<button id="botao_adicionar_produtos">
 <img class="display_flex" id="tamanho_icone_adicionar" src="../assets/icons/mais.webp" alt="Botão de adicionar" <p>Adicionar produtos</p>
-</div>
+</button>
+</a>
 </header>
 
 <main>
