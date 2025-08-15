2c662823e10a377c04fc34025424f182fe1e48ce
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-08-15 10:07:43 -0300

    Terminado o header da aba de produtos e começo do desenvolvimento do main.

--- public/produtos.php
+++ public/produtos.php
@@ -14,23 +14,22 @@
 </head>
 <body>
 <header>
-<div class="header_background">
+<div  class="header_background">
     <div class="posicao_seta">
         <img src="../assets/icons/seta_voltar.png" alt="Seta de voltar">
     </div>
 <img class="tamanho_logo" src="../assets/logos/logo_padaria.png" alt="Logo da padaria">
-<div class="botao_produtos">
-<img class="tamanho_icones_produtos" src="../assets/icons/carrinho.png" alt="Carrinho">
+<div class="botao_ver_carrinho">
+<img id="tamanho_icone_carrinho" src="../assets/icons/carrinho.png" alt="Carrinho">
 <p>Ver carrinho</p>
-
-<div class="botao_produtos">
-<img class="tamanho_icones_produtos" src="../assets/icons/mais.png" alt="Botão de adicionar">
-<p>Adicionar produtos</p>
-
+</div>
+<div class="botao_adicionar_produtos">
+<img class="display_flex" id="tamanho_icone_adicionar" src="../assets/icons/mais.webp" alt="Botão de adicionar" <p>Adicionar produtos</p>
+</div>
 </header>
 
 <main>
-
+<img id="imagem_produtos" src="../assets/icons/imagem_produtos.png" alt="Imagem de Produtos">
 </main>
 
 <footer>
